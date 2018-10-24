5ad9bbd0900b01b898f5468751f301e73cbaca9d
Kate Thomson <2191968t@student.gla.ac.uk>
2018-10-24 10:34:50 +0100

    Update js/index.js

--- js/index.js
+++ js/index.js
@@ -20,4 +20,4 @@ options = {
 vrvToolkit.setOptions(options)
 
 /* Show the score */
-showScore('score', 'data/example.mei') // <-- CHANGE THIS TO YOUR FILE
+showScore('score', 'data/I Giorni - Piano.mei') // <-- CHANGE THIS TO YOUR FILE
